83d76ee28df9f168f88b2759fd4a8183fc945f51
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2025-03-28 08:50:30 +0200

    Add files via upload

---
+++ OnlineNotePad/Chapter1/OnlineNotePad/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineNotePad.Models;
+
+namespace OnlineNotePad.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Document> Documents { get; set; } 
+    }
+}
