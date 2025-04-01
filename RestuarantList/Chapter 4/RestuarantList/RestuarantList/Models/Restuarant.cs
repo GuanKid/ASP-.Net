99a02752a5aa4f03b5335d35438fb84bca8d3cd8
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2025-04-01 14:41:29 +0200

    Add files via upload

---
+++ RestuarantList/Chapter 4/RestuarantList/RestuarantList/Models/Restuarant.cs
@@ -0,0 +1,14 @@
+﻿namespace RestuarantList.Models
+{
+    public class Restuarant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string Address { get; set; }
+
+        public List<RestuarantDish>? RestuarantDishes { get; set; }
+    }
+}
